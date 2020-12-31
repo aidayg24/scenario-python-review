ba9fb07f5fb8b2b0ada353ca135aee673c8777bb
aidayg24 <aidarostamee221bb@gmail.com>
2020-12-31 08:31:15 +0330

    111099 scenario python review

--- iris link.py
+++ iris link.py
@@ -3,8 +3,9 @@ import csv
 import urllib
 from urllib import request
 
-
 url = "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv"
 stream = urllib.request.urlopen(url)
 csv_file = csv.reader(codecs.iterdecode(stream, 'utf-8'))
-
+# Return a reader object which will iterate over lines in the given csv file.
+# for row in csv_file:
+#     print(row)
